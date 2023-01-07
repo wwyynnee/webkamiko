c86957e1215771d9f337f2d214423f2ebb7d9ed5
Wynne <76490156+wwyynnee@users.noreply.github.com>
2023-01-07 07:38:46 +0000

    Добавление элементов

--- script.js
+++ script.js
@@ -91,6 +91,250 @@ const blockActive23 = () => {
   $(".cmd__23 details summary div svg").toggleClass("fillActive");
   $(".cmd__23 details summary svg").toggleClass("transformActive");
 }
+const blockActive24 = () => {
+  $(".cmd__24 details summary div svg").toggleClass("fillActive");
+  $(".cmd__24 details summary svg").toggleClass("transformActive");
+}
+const blockActive25 = () => {
+  $(".cmd__25 details summary div svg").toggleClass("fillActive");
+  $(".cmd__25 details summary svg").toggleClass("transformActive");
+}
+const blockActive26 = () => {
+  $(".cmd__26 details summary div svg").toggleClass("fillActive");
+  $(".cmd__26 details summary svg").toggleClass("transformActive");
+}
+const blockActive27 = () => {
+  $(".cmd__27 details summary div svg").toggleClass("fillActive");
+  $(".cmd__27 details summary svg").toggleClass("transformActive");
+}
+const blockActive28 = () => {
+  $(".cmd__28 details summary div svg").toggleClass("fillActive");
+  $(".cmd__28 details summary svg").toggleClass("transformActive");
+}
+const blockActive29 = () => {
+  $(".cmd__29 details summary div svg").toggleClass("fillActive");
+  $(".cmd__29 details summary svg").toggleClass("transformActive");
+}
+const blockActive30 = () => {
+  $(".cmd__30 details summary div svg").toggleClass("fillActive");
+  $(".cmd__30 details summary svg").toggleClass("transformActive");
+}
+const blockActive31 = () => {
+  $(".cmd__31 details summary div svg").toggleClass("fillActive");
+  $(".cmd__31 details summary svg").toggleClass("transformActive");
+}
+const blockActive32 = () => {
+  $(".cmd__32 details summary div svg").toggleClass("fillActive");
+  $(".cmd__32 details summary svg").toggleClass("transformActive");
+}
+const blockActive33 = () => {
+  $(".cmd__33 details summary div svg").toggleClass("fillActive");
+  $(".cmd__33 details summary svg").toggleClass("transformActive");
+}
+const blockActive34 = () => {
+  $(".cmd__34 details summary div svg").toggleClass("fillActive");
+  $(".cmd__34 details summary svg").toggleClass("transformActive");
+}
+const blockActive35 = () => {
+  $(".cmd__35 details summary div svg").toggleClass("fillActive");
+  $(".cmd__35 details summary svg").toggleClass("transformActive");
+}
+const blockActive36 = () => {
+  $(".cmd__36 details summary div svg").toggleClass("fillActive");
+  $(".cmd__36 details summary svg").toggleClass("transformActive");
+}
+const blockActive37 = () => {
+  $(".cmd__37 details summary div svg").toggleClass("fillActive");
+  $(".cmd__37 details summary svg").toggleClass("transformActive");
+}
+const blockActive38 = () => {
+  $(".cmd__38 details summary div svg").toggleClass("fillActive");
+  $(".cmd__38 details summary svg").toggleClass("transformActive");
+}
+const blockActive39 = () => {
+  $(".cmd__39 details summary div svg").toggleClass("fillActive");
+  $(".cmd__39 details summary svg").toggleClass("transformActive");
+}
+const blockActive40 = () => {
+  $(".cmd__40 details summary div svg").toggleClass("fillActive");
+  $(".cmd__40 details summary svg").toggleClass("transformActive");
+}
+const blockActive41 = () => {
+  $(".cmd__41 details summary div svg").toggleClass("fillActive");
+  $(".cmd__41 details summary svg").toggleClass("transformActive");
+}
+const blockActive42 = () => {
+  $(".cmd__42 details summary div svg").toggleClass("fillActive");
+  $(".cmd__42 details summary svg").toggleClass("transformActive");
+}
+const blockActive43 = () => {
+  $(".cmd__43 details summary div svg").toggleClass("fillActive");
+  $(".cmd__43 details summary svg").toggleClass("transformActive");
+}
+const blockActive44 = () => {
+  $(".cmd__44 details summary div svg").toggleClass("fillActive");
+  $(".cmd__44 details summary svg").toggleClass("transformActive");
+}
+const blockActive45 = () => {
+  $(".cmd__45 details summary div svg").toggleClass("fillActive");
+  $(".cmd__45 details summary svg").toggleClass("transformActive");
+}
+const blockActive46 = () => {
+  $(".cmd__46 details summary div svg").toggleClass("fillActive");
+  $(".cmd__46 details summary svg").toggleClass("transformActive");
+}
+const blockActive47 = () => {
+  $(".cmd__47 details summary div svg").toggleClass("fillActive");
+  $(".cmd__47 details summary svg").toggleClass("transformActive");
+}
+const blockActive48 = () => {
+  $(".cmd__48 details summary div svg").toggleClass("fillActive");
+  $(".cmd__48 details summary svg").toggleClass("transformActive");
+}
+const blockActive49 = () => {
+  $(".cmd__49 details summary div svg").toggleClass("fillActive");
+  $(".cmd__49 details summary svg").toggleClass("transformActive");
+}
+const blockActive50 = () => {
+  $(".cmd__50 details summary div svg").toggleClass("fillActive");
+  $(".cmd__50 details summary svg").toggleClass("transformActive");
+}
+const blockActive51 = () => {
+  $(".cmd__51 details summary div svg").toggleClass("fillActive");
+  $(".cmd__51 details summary svg").toggleClass("transformActive");
+}
+const blockActive52 = () => {
+  $(".cmd__52 details summary div svg").toggleClass("fillActive");
+  $(".cmd__52 details summary svg").toggleClass("transformActive");
+}
+const blockActive53 = () => {
+  $(".cmd__53 details summary div svg").toggleClass("fillActive");
+  $(".cmd__53 details summary svg").toggleClass("transformActive");
+}
+const blockActive54 = () => {
+  $(".cmd__54 details summary div svg").toggleClass("fillActive");
+  $(".cmd__54 details summary svg").toggleClass("transformActive");
+}
+const blockActive55 = () => {
+  $(".cmd__55 details summary div svg").toggleClass("fillActive");
+  $(".cmd__55 details summary svg").toggleClass("transformActive");
+}
+const blockActive56 = () => {
+  $(".cmd__56 details summary div svg").toggleClass("fillActive");
+  $(".cmd__56 details summary svg").toggleClass("transformActive");
+}
+const blockActive57 = () => {
+  $(".cmd__57 details summary div svg").toggleClass("fillActive");
+  $(".cmd__57 details summary svg").toggleClass("transformActive");
+}
+const blockActive58 = () => {
+  $(".cmd__58 details summary div svg").toggleClass("fillActive");
+  $(".cmd__58 details summary svg").toggleClass("transformActive");
+}
+const blockActive59 = () => {
+  $(".cmd__59 details summary div svg").toggleClass("fillActive");
+  $(".cmd__59 details summary svg").toggleClass("transformActive");
+}
+const blockActive60 = () => {
+  $(".cmd__60 details summary div svg").toggleClass("fillActive");
+  $(".cmd__60 details summary svg").toggleClass("transformActive");
+}
+const blockActive61 = () => {
+  $(".cmd__61 details summary div svg").toggleClass("fillActive");
+  $(".cmd__61 details summary svg").toggleClass("transformActive");
+}
+const blockActive62 = () => {
+  $(".cmd__62 details summary div svg").toggleClass("fillActive");
+  $(".cmd__62 details summary svg").toggleClass("transformActive");
+}
+const blockActive63 = () => {
+  $(".cmd__63 details summary div svg").toggleClass("fillActive");
+  $(".cmd__63 details summary svg").toggleClass("transformActive");
+}
+const blockActive64 = () => {
+  $(".cmd__64 details summary div svg").toggleClass("fillActive");
+  $(".cmd__64 details summary svg").toggleClass("transformActive");
+}
+const blockActive65 = () => {
+  $(".cmd__65 details summary div svg").toggleClass("fillActive");
+  $(".cmd__65 details summary svg").toggleClass("transformActive");
+}
+const blockActive66 = () => {
+  $(".cmd__66 details summary div svg").toggleClass("fillActive");
+  $(".cmd__66 details summary svg").toggleClass("transformActive");
+}
+const blockActive67 = () => {
+  $(".cmd__67 details summary div svg").toggleClass("fillActive");
+  $(".cmd__67 details summary svg").toggleClass("transformActive");
+}
+const blockActive68 = () => {
+  $(".cmd__68 details summary div svg").toggleClass("fillActive");
+  $(".cmd__68 details summary svg").toggleClass("transformActive");
+}
+const blockActive69 = () => {
+  $(".cmd__69 details summary div svg").toggleClass("fillActive");
+  $(".cmd__69 details summary svg").toggleClass("transformActive");
+}
+const blockActive70 = () => {
+  $(".cmd__70 details summary div svg").toggleClass("fillActive");
+  $(".cmd__70 details summary svg").toggleClass("transformActive");
+}
+const blockActive71 = () => {
+  $(".cmd__71 details summary div svg").toggleClass("fillActive");
+  $(".cmd__71 details summary svg").toggleClass("transformActive");
+}
+const blockActive72 = () => {
+  $(".cmd__72 details summary div svg").toggleClass("fillActive");
+  $(".cmd__72 details summary svg").toggleClass("transformActive");
+}
+const blockActive73 = () => {
+  $(".cmd__73 details summary div svg").toggleClass("fillActive");
+  $(".cmd__73 details summary svg").toggleClass("transformActive");
+}
+const blockActive74 = () => {
+  $(".cmd__74 details summary div svg").toggleClass("fillActive");
+  $(".cmd__74 details summary svg").toggleClass("transformActive");
+}
+const blockActive75 = () => {
+  $(".cmd__75 details summary div svg").toggleClass("fillActive");
+  $(".cmd__75 details summary svg").toggleClass("transformActive");
+}
+const blockActive76 = () => {
+  $(".cmd__76 details summary div svg").toggleClass("fillActive");
+  $(".cmd__76 details summary svg").toggleClass("transformActive");
+}
+const blockActive77 = () => {
+  $(".cmd__77 details summary div svg").toggleClass("fillActive");
+  $(".cmd__77 details summary svg").toggleClass("transformActive");
+}
+const blockActive78 = () => {
+  $(".cmd__78 details summary div svg").toggleClass("fillActive");
+  $(".cmd__78 details summary svg").toggleClass("transformActive");
+}
+const blockActive79 = () => {
+  $(".cmd__79 details summary div svg").toggleClass("fillActive");
+  $(".cmd__79 details summary svg").toggleClass("transformActive");
+}
+const blockActive80 = () => {
+  $(".cmd__80 details summary div svg").toggleClass("fillActive");
+  $(".cmd__80 details summary svg").toggleClass("transformActive");
+}
+const blockActive81 = () => {
+  $(".cmd__81 details summary div svg").toggleClass("fillActive");
+  $(".cmd__81 details summary svg").toggleClass("transformActive");
+}
+const blockActive82 = () => {
+  $(".cmd__82 details summary div svg").toggleClass("fillActive");
+  $(".cmd__82 details summary svg").toggleClass("transformActive");
+}
+const blockActive83 = () => {
+  $(".cmd__83 details summary div svg").toggleClass("fillActive");
+  $(".cmd__83 details summary svg").toggleClass("transformActive");
+}
+const blockActive84 = () => {
+  $(".cmd__84 details summary div svg").toggleClass("fillActive");
+  $(".cmd__84 details summary svg").toggleClass("transformActive");
+}
 
 // Вкладки
 const pink = "#e9cdeb";
@@ -98,18 +342,18 @@ const pink = "#e9cdeb";
 // Все
 $(".all_btn").on("click", () => {
   $(".all_btn").attr("style", `background-color: ${pink} !important; color: black !important`);
-  $(".main_btn, .rp_btn, .img_btn").css("backgroundColor", "#242424")
-  $(".main_btn, .rp_btn, .img_btn").css("color", "#777")
-  $(".basic, .rp, .img").show()
+  $(".main_btn, .rp_btn, .img_btn, .nsfw_btn").css("backgroundColor", "#242424")
+  $(".main_btn, .rp_btn, .img_btn, .nsfw_btn").css("color", "#777")
+  $(".basic, .rp, .img, .nsfw").show()
   $(".spanActive").text("Все команды (▽◕ ᴥ ◕▽)")
 })
 
 // Основное
 $(".main_btn").on("click", () => {
   $(".main_btn").attr("style", `background-color: ${pink} !important; color: black !important`);
-  $(".all_btn, .rp_btn, .img_btn").css("backgroundColor", "#242424")
-  $(".all_btn, .rp_btn, .img_btn").css("color", "#777")
-  $(".rp, .img").hide()
+  $(".all_btn, .rp_btn, .img_btn, .nsfw_btn").css("backgroundColor", "#242424")
+  $(".all_btn, .rp_btn, .img_btn, .nsfw_btn").css("color", "#777")
+  $(".rp, .img, .nsfw").hide()
   $(".basic").show()
   $(".spanActive").text("Основные команды бота (✯◡✯)")
 })
@@ -117,9 +361,9 @@ $(".main_btn").on("click", () => {
 // Ролевые
 $(".rp_btn").on("click", () => {
   $(".rp_btn").attr("style", `background-color: ${pink} !important; color: black !important`);
-  $(".all_btn, .main_btn, .img_btn").css("backgroundColor", "#242424")
-  $(".all_btn, .main_btn, .img_btn").css("color", "#777")
-  $(".basic, .img").hide()
+  $(".all_btn, .main_btn, .img_btn, .nsfw_btn").css("backgroundColor", "#242424")
+  $(".all_btn, .main_btn, .img_btn, .nsfw_btn").css("color", "#777")
+  $(".basic, .img, .nsfw").hide()
   $(".rp").show()
   $(".spanActive").text("Команды для ролевых игр ʕ ᵔᴥᵔ ʔ")
 })
@@ -127,11 +371,21 @@ $(".rp_btn").on("click", () => {
 // Картинки
 $(".img_btn").on("click", () => {
   $(".img_btn").attr("style", `background-color: ${pink} !important; color: black !important`);
-  $(".all_btn, .main_btn, .rp_btn").css("backgroundColor", "#242424")
-  $(".all_btn, .main_btn, .rp_btn").css("color", "#777")
-  $(".basic, .rp").hide()
+  $(".all_btn, .main_btn, .rp_btn, .nsfw_btn").css("backgroundColor", "#242424")
+  $(".all_btn, .main_btn, .rp_btn, .nsfw_btn").css("color", "#777")
+  $(".basic, .rp, .nsfw").hide()
   $(".img").show()
   $(".spanActive").text("Всеми любимые картинки ツ")
+})
+
+// NSFW-картинки
+$(".nsfw_btn").on("click", () => {
+  $(".nsfw_btn").attr("style", `background-color: ${pink} !important; color: black !important`);
+  $(".all_btn, .main_btn, .img_btn, .rp_btn").css("backgroundColor", "#242424")
+  $(".all_btn, .main_btn, .img_btn, .rp_btn").css("color", "#777")
+  $(".basic, .img, .rp").hide()
+  $(".nsfw").show()
+  $(".spanActive").text("Нам это нравится~")
 })
 
 // Меню
